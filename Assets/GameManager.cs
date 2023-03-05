d7d78fe5d7a2abb67f201869e754ac4c8b2304fe
TOMIO NAGANO <Temujin142857@gmail.com>
2023-03-05 06:50:23 -0500

    to be screenshotted

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,34 +8,35 @@ public class GameManager : MonoBehaviour
     public static string[] piece_tags;
     public GameObject White_Square;
     public GameObject Vertical_Line;
-    public GameObject Horizantal_Line;
+    public  GameObject Horizantal_Line;
     public GameObject Purple_Square;
     public GameObject Green_Square;
     public GameObject Square_Pieces;
 
-    public int lengthMax=10;    
-    public int boardBottomCornerx=-5;
-    public int boardBottomCornery=-5;
-    public int pieceRowMax=3;
+    public static int lengthMax=10;    
+    public static int boardBottomCornerx=-5;
+    public static int boardBottomCornery=-3;
+    public static int level;
+    public static int pieceRowMax=3;
     public int pieceColumnMax=3;
 
-    public int leftBorderShop=7;
-    public int lowerBorderShop=-5;
+    public static int leftBorderShop=7;
+    public static int lowerBorderShop=-3;
 
     //spaces out the pieces in the shop, distance is bottom of lower piece to bottom of next piece
-    public int verticalInShopInterval=5;
+    public static int verticalInShopInterval=4;
 
     //prebuilt boards will be stored in this
     //int values: 0 inidactes empty, 1 indicates a plain square, 2=combo square, cover all of these for a bonus, prob money
-    private int[][] boardLVL1=new int[][]{new int[]{1,2,0,1,1,1,1,0},new int[]{1,1,1,1,1,0,1},new int[]{1,1,1,0,2,1,1},new int[]{1,1,1,0,0,1,1},new int[]{0,1,1,0,2,1,1,1},new int[]{1,1,1,0,1,1,0,1}};
-    private int[][] piece1LVL1=new int[][]{new int[]{0,0,0},new int[]{0,1,1},new int[]{1,1,0}};
-    private int[][] piece2LVL1=new int[][]{new int[]{0,0,1},new int[]{1,1,1},new int[]{1,0,0}};
+    private static int[][] boardLVL1=new int[][]{new int[]{1,2,0,1,1,1,1,0},new int[]{1,1,1,1,1,0,1},new int[]{1,1,1,0,2,1,1},new int[]{1,1,1,0,0,1,1},new int[]{0,1,1,0,2,1,1,1},new int[]{1,1,1,0,1,1,0,1}};
+    public int[][] piece1LVL1=new int[][]{new int[]{0,0,0},new int[]{0,1,1},new int[]{1,1,0}};
+    public int[][] piece2LVL1=new int[][]{new int[]{0,0,1},new int[]{1,1,1},new int[]{1,0,0}};
     //private int[][] piece3LVL1=new int[][]{new int[]{0,1,1,1},new int[]{1,1,1,0},new int[]{1,1,0,0},new int[]{0,1,0,0}};
     //for now two pieces, eco piece and non eco piece
-
-    private int[][][] piecesLVL1;
+    
+    public int[][][] piecesLVL1;
     private int[][] boardLVL2=new int[][]{};
-    private int[][][] allBoards;
+    private static int[][][] allBoards;
     private int[][][][] allPiecesAllLevels;
     
     private static GameObject[][] pieces;
@@ -44,8 +45,59 @@ public class GameManager : MonoBehaviour
     private static List<GameObject> linesH=new List<GameObject>();
     private static float[][] deltaLineXY=new float[4][];
     
-    private int nextTagNum=0;
+    private static int nextTagNum=0;
     
+
+    //exit
+    public void exit(){
+        Debug.Log("exit");
+    }
+
+    //checks to see if the piece collides with a white tile
+    public static bool checkCollision(string tag){
+        foreach(GameObject[] piece in pieces){
+            if(piece!=null&&piece[0].tag.Equals(tag)){
+                foreach(GameObject square in piece){
+                    for(int i=0;i<lengthMax;i++){
+                        for (int j = 0; j < lengthMax; j++) {
+                            int x=i+boardBottomCornerx;
+                            int y=j+boardBottomCornery;
+                            if (blockAt(i,j,level)==0){
+                                if(square.transform.position.x==x&&square.transform.position.y==y){
+                                    relocatePiece(tag);
+                                    return true;                                                                   
+                                }
+                            }
+                        }
+                    }                    
+                }
+            }
+        }
+        return false;        
+    }
+
+    public static bool checkCollision2(string tag){
+        foreach(GameObject[] piece in pieces){
+            if(piece!=null&&piece[0].tag.Equals(tag)){
+                foreach(GameObject square in piece){
+                    for(int i=0;i<lengthMax;i++){
+                        for (int j = 0; j < lengthMax; j++) {
+                            int x=i+boardBottomCornerx;
+                            int y=j+boardBottomCornery;
+                            if (blockAt(i,j,level)==1){
+                                if(square.transform.position.x==x&&square.transform.position.y==y){                                   
+                                    return true;                                                                   
+                                }
+                            }
+                        }
+                    }                    
+                }
+            }
+        }
+        return false;        
+    }
+
+    //sets the position of each peice and line relative to the cursor
     public static void setDelta(Vector2 mousePosition,string tag){
         deltaXY[0]=new float[getLength(pieces)];
         deltaXY[1]=new float[getLength(pieces)];
@@ -55,7 +107,8 @@ public class GameManager : MonoBehaviour
         deltaLineXY[3]=new float[linesV.Count];
         int i=0;      
         foreach(GameObject[] piece in pieces){
-            foreach(GameObject square in piece){
+            if(piece==null)continue;
+            foreach(GameObject square in piece){                
                 if(square.tag.Equals(tag)){         
                     deltaXY[0][i]=mousePosition.x-square.transform.position.x;
                     deltaXY[1][i]=mousePosition.y-square.transform.position.y;               
@@ -83,9 +136,11 @@ public class GameManager : MonoBehaviour
         
     }
 
+    //has the piece follow the cursor
     public static void dragPiece(Vector2 mousePosition,string tag){  
         int i=0;      
         foreach(GameObject[] piece in pieces){
+            if(piece==null)continue;
             foreach(GameObject square in piece){
                 if(square.tag.Equals(tag)){                    
                     square.transform.position=new Vector2((float)System.Math.Ceiling(mousePosition.x+deltaXY[0][i]),(float)System.Math.Ceiling(mousePosition.y+deltaXY[1][i]));                
@@ -117,7 +172,7 @@ public class GameManager : MonoBehaviour
         piecesLVL1=new int[][][]{piece1LVL1,piece2LVL1};        
         allPiecesAllLevels=new int[][][][]{piecesLVL1};
         lengthMax=boardLVL1.Length;
-        pieces=new GameObject[piecesLVL1.Length][];
+        pieces=new GameObject[piecesLVL1.Length*20][];
         BuildBoard(0);    
         BuildPieces(allPiecesAllLevels[0]);  
     }
@@ -158,7 +213,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private int blockAt(int x, int y, int level){
+    private  static int blockAt(int x, int y, int level){
            return allBoards[level][x][y];
     }
 
@@ -176,6 +231,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //overload of the previous function, gives the lines the appropriate piece tag
     private void surroundBlockwithLines(int x, int y, string tag){
         float half=(float)0.5;
         for (int i = 0; i < 2; i++){
@@ -191,10 +247,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
+    //builds all the pieces needed in a level
     public void BuildPieces(int[][][] piecesForAllLevels){        
-        foreach(int[][] piece in piecesForAllLevels){
-            pieces[nextTagNum]=new GameObject[numBlocksinPiece(piece)];
+        foreach(int[][] piece in piecesForAllLevels){        
             BuildPiece(piece);
         }
         setPiecesInShop();
@@ -202,7 +257,9 @@ public class GameManager : MonoBehaviour
 
     //builds a piece, as long as movement is handled in such a way thet their relative distance remains the same
     //the array should look like a piece
-    private void BuildPiece(int[][] piece){
+    public void BuildPiece(int[][] piece){
+        Debug.Log(nextTagNum);
+        pieces[nextTagNum]=new GameObject[numBlocksinPiece(piece)];
         int k=0;
         string nextTag="piece"+nextTagNum;        
         for (int i = 0; i < pieceRowMax; i++){
@@ -220,10 +277,12 @@ public class GameManager : MonoBehaviour
         nextTagNum++;
     }
 
+    //moves the pieces from the centre of the board to the shop
     private void setPiecesInShop(){
         int i=0;
         foreach(GameObject[] piece in pieces){
-            foreach(GameObject square in piece){
+            if(piece==null)continue;
+            foreach(GameObject square in piece){                
                 square.transform.position = new Vector2(square.transform.position.x+leftBorderShop, square.transform.position.y+lowerBorderShop+i*verticalInShopInterval);
             }
             foreach(GameObject line in linesH){
@@ -240,6 +299,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //returns the number of squares in a piece
     private int numBlocksinPiece(int[][] piece){
         int result=0;
         foreach(int[] row in piece){
@@ -252,11 +312,39 @@ public class GameManager : MonoBehaviour
         return result;
     }
 
+
     private static int getLength(GameObject[][] array2D){
         int result=0;
         foreach(GameObject[] piece in pieces){
+            if(piece==null)continue;
             result+=piece.Length;
         }
         return result;
     }
+
+    //pushes a piece of the board when it is set on a white square
+    private static void relocatePiece(string tag){
+        int i= tag.ToCharArray()[5]-'0';
+        foreach(GameObject[] piece in pieces){
+            if(piece==null)continue;
+            if(piece[0].tag.Equals(tag)){
+                foreach(GameObject square in piece){
+                    square.transform.position = new Vector2(square.transform.position.x+leftBorderShop, square.transform.position.y+i);
+                }
+                foreach(GameObject line in linesH){
+                    if(piece[0].tag.Equals(line.tag)){
+                        line.transform.position=new Vector2(line.transform.position.x+leftBorderShop, line.transform.position.y+i);
+                    }
+                }
+                foreach(GameObject line in linesV){
+                    if(piece[0].tag.Equals(line.tag)){
+                        line.transform.position=new Vector2(line.transform.position.x+leftBorderShop, line.transform.position.y+i);
+                    }
+                }
+            }
+        }
+    }
+    
+
+
 }
--- Assets/Piece_Controller.cs
+++ Assets/Piece_Controller.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Piece_Controller : MonoBehaviour
 {
     public float differenceFromOrigin=0;
-
+    
     public bool isBeingDragged = false;
+    
+    public int covered=0;
     
     private Collider2D collider2D;
     private bool canMove=false;
+    
+    MonoBehaviour manager;
     
 
     // Start is called before the first frame update
     void Start()
     {
         collider2D=GetComponent<Collider2D>();   
+        
     }
 
     // Update is called once per frame
@@ -25,11 +30,9 @@ public class Piece_Controller : MonoBehaviour
         if(isBeingDragged){
            GameManager.dragPiece(mousePosition,gameObject.tag);
         }
-        if(Input.GetMouseButtonDown(0)){
-            
+        if(Input.GetMouseButtonDown(0)){            
             if (collider2D == Physics2D.OverlapPoint(mousePosition)){
             canMove=true;
-            Debug.Log("_______________________SHOULD BE ONCE___________________");
             GameManager.setDelta(mousePosition,gameObject.tag);
             }
             else canMove=false;
@@ -39,8 +42,18 @@ public class Piece_Controller : MonoBehaviour
         if(Input.GetMouseButtonUp(0)){
             canMove=false;
             isBeingDragged=false;
-        }
+            if(GameManager.checkCollision(gameObject.tag)){
+                int i=tag.ToCharArray()[5]-'0';                
+            }
+            if(GameManager.checkCollision2(gameObject.tag)){
+                covered++;   
+                if(covered==2){
+                    GameObject.FindObjectOfType<GameManager>().exit();
+                }            
+            }
+            //else GameObject.FindObjectOfType<GameManager>().BuildPiece(GameObject.FindObjectOfType<GameManager>().piecesLVL1[1]);
 
+        }
     }
 
     
